8b0d47f9b355bcfdf19d398e550cd09a74bb29b5
JavierSalgado01 <javiersalgado980@gmail.com>
2024-11-13 12:10:29 -0300

    plantilla html

--- django/proyecto_construccion/proyecto_be/views.py
+++ django/proyecto_construccion/proyecto_be/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Template, Context
 
-# Create your views here.
+def plantilla_html(request):
+    return render(request, 'index.html')
